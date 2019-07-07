73933f6cf9c3bf5eee216ef0c256e4ee12f952a4
qababu <brahma457@gmail.com>
2019-07-07 11:26:34 +0530

    program to find verify the given mobile num starts with 7, 8 or 9

---
+++ src/Numerics/mobileNumStartingDigit.java
@@ -0,0 +1,30 @@
+package Numerics;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class mobileNumStartingDigit  {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 10 digit mobile number: ");
+
+        long mobile_num = sc.nextLong();
+
+        System.out.println("Given mobile number: "+mobile_num);
+        //converting Long to String type
+        String s = Long.toString(mobile_num);
+
+        String regEx = "^[789][0-9]{9}";
+        //Matching regular expression with given mobile number
+        boolean m = Pattern.matches(regEx,s);
+
+        if(m){
+            System.out.println("Given mobile number is valid");
+        }else{
+            System.out.println("Given mobile number is invalid");
+        }
+    }
+}
