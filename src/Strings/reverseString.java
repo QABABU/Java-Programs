bc533141562f3a4a590408b7a3497f9f59a5ec26
qababu <brahma457@gmail.com>
2019-09-16 08:38:57 +0530

    reverse a string updated

--- src/Strings/reverseString.java
+++ src/Strings/reverseString.java
@@ -1,23 +1,30 @@
 package Strings;
 
+import java.util.Scanner;
+
 public class reverseString {
 
     public static void main(String[] args) {
 
-        String reversed = reverse("qababu.com");
+        Scanner scanner = new Scanner(System.in);
+        //reading input string
+        String inputString = scanner.next();
+
+        scanner.close();
+
+        String reversed = reverse(inputString);
 
         System.out.println("The reversed string is: "+reversed);
 
     }
 
+    //method to reverse given string
     private static String reverse(String str){
-
         String reverse="";
-
+        //converting given string character array
         char[] ch = str.toCharArray();
-
         for (char c: ch) {
-
+            //adding each character to reverse variable in reversed order
             reverse = c + reverse;
         }
 
